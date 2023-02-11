63d963cdda04f6444ab07ad8bb3e0fbf1d150b71
MarcosDominguezVega <228@cifpceuta.es>
2023-02-11 23:51:34 +0100

    Mira esto

---
+++ Prueba/index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="favicon" href=""/> 
+    <title>Prueba</title>
+</head>
+<body>
+    <div class="sketchfab-embed-wrapper"> 
+        <iframe title="Chihuahua fighting machine" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1610f180e88d435eb4ebddf9b950b021/embed"> </iframe> 
+        <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> 
+            <a href="https://sketchfab.com/3d-models/chihuahua-fighting-machine-1610f180e88d435eb4ebddf9b950b021?utm_medium=embed&utm_campaign=share-popup&utm_content=1610f180e88d435eb4ebddf9b950b021" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Chihuahua fighting machine </a> 
+                by <a href="https://sketchfab.com/uliszs.3d?utm_medium=embed&utm_campaign=share-popup&utm_content=1610f180e88d435eb4ebddf9b950b021" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Uliszs.3d </a> 
+                    on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=1610f180e88d435eb4ebddf9b950b021" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
+        </p>
+    </div>
+</body>
+</html>
+
